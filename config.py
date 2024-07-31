9873c3cc129955c42d1cfea302f7d1c62eb3555a
Rhul9610 <babykidsshow8@gmail.com>
2024-07-31 12:45:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,16 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "20373830"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "f142d82b65e173b658cb063014350d84")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "Api id - 20373830
+
+Api hash - f142d82b65e173b658cb063014350d84
+
+Bot token - 7286722612:AAGsLvPoXEsIdHZ_hJvrWwngwOMk6tIACAI
+
+Mongodb Database - mongodb+srv://babykidsshow8:0taFx1enCzgRTaus@axomyt.v5eqtwd.mongodb.net/?retryWrites=true&w=majority&appName=Axomyt")
